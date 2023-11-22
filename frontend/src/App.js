42cb3ce756b441560cc1502416daec18e518b6e4
tejasvinu <tejasvinu@gmail.com>
2023-11-22 22:25:32 +0530

    added home , projects and contact pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
+// Import necessary components
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/home';
-import './App.css';
+import Projects from './components/projects';
+import Contact from './components/contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -8,19 +8,21 @@ const Home = () => {
     <div className="container mt-5">
       <div className="row">
         <div className="col-lg-8">
-          <h1 className="display-4">Your Name</h1>
+          <h1 className="display-4 fw-bold">Your Name</h1>
           <p className="lead">Web Developer | Designer | Problem Solver</p>
           <hr className="my-4" />
-          <p>
+          <p className="fs-5">
             Welcome to my portfolio! I am a passionate web developer with a focus on creating
             user-friendly and elegant solutions. Explore my projects and skills below.
           </p>
-          <Link to="/projects" className="btn btn-primary btn-lg mr-2">
-            View Projects
-          </Link>
-          <Link to="/contact" className="btn btn-secondary btn-lg">
-            Contact Me
-          </Link>
+          <div className="d-flex">
+            <Link to="/projects" className="btn btn-primary btn-lg me-2">
+              View Projects
+            </Link>
+            <Link to="/contact" className="btn btn-secondary btn-lg">
+              Contact Me
+            </Link>
+          </div>
         </div>
         <div className="col-lg-4">
           <img
